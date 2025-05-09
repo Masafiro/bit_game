e293226fd94997d64553c457dfacab92facd3f3c
Masafiro <141376324+Masafiro@users.noreply.github.com>
2025-05-09 15:22:54 +0900

    絶対パスが正しく動作するようにbasePathを指定

--- src/next.config.ts
+++ src/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/bit_game', // GitHub Pages のリポジトリ名を指定
+  assetPrefix: '/bit_game', // 静的ファイルのパスを修正
 };
 
 export default nextConfig;
