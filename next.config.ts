3dead75cd0c2226d36c1294570215a890f8248df
Masafiro <141376324+Masafiro@users.noreply.github.com>
2025-05-16 15:06:55 +0900

    パスの修正

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 /* 公開時のサブディレクトリ */
-const SUB_DIRECTORY = "/bit_game/out";
+const SUB_DIRECTORY = "/bit_game";
 
 /* 本番環境と開発環境の分岐用のフラグ */
 const isProd = process.env.NODE_ENV === "production";
