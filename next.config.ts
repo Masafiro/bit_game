847a7018bab774935b7ab78b60ac91a92293831f
Masafiro <141376324+Masafiro@users.noreply.github.com>
2025-05-16 13:05:39 +0900

    pathの修正

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,13 @@ import type { NextConfig } from "next";
 /* 公開時のサブディレクトリ */
 const SUB_DIRECTORY = "/bit_game";
 
+/* 本番環境と開発環境の分岐用のフラグ */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: SUB_DIRECTORY,
-  assetPrefix: SUB_DIRECTORY,
+  basePath: isProd ? SUB_DIRECTORY : "",
+  assetPrefix: isProd ? SUB_DIRECTORY : "",
 };
-
-// /* 本番環境と開発環境の分岐用のフラグ */
-// const isProd = process.env.NODE_ENV === "production";
-
-// const nextConfig: NextConfig = {
-//   output: "export",
-//   basePath: isProd ? SUB_DIRECTORY : "",
-//   assetPrefix: isProd ? SUB_DIRECTORY : "",
-// };
 
 export default nextConfig;
