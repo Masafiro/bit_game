7a0a66bade7a24183b013a1ccfc7869a1c4f5568
Masafiro <141376324+Masafiro@users.noreply.github.com>
2025-05-16 13:57:35 +0900

    postbuildコマンドを追加

---
+++ scripts/restructureOut.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
+const outDir = path.join(__dirname, "../out");
+const targetDir = path.join(outDir, "bit_game");
+
+// 必要なディレクトリを作成
+if (!fs.existsSync(targetDir)) {
+  fs.mkdirSync(targetDir, { recursive: true });
+}
+
+// 移動対象のファイルとディレクトリ
+const itemsToMove = ["_next", "favicon.ico"];
+
+// out 内の指定されたファイルやディレクトリを bit_game ディレクトリに移動
+itemsToMove.forEach((item) => {
+  const itemPath = path.join(outDir, item);
+  const targetPath = path.join(targetDir, item);
+
+  if (fs.existsSync(itemPath)) {
+    fs.renameSync(itemPath, targetPath);
+    console.log(`Moved ${item} to ${targetDir}`);
+  } else {
+    console.warn(`Warning: ${item} does not exist in ${outDir}`);
+  }
+});
+
+console.log("Restructured out directory!");
